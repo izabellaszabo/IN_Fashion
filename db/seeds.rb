c2b21b48e482c6960f42bad7e9b5aee2186ffca5
enterpriseboy <niall.maguire@topmail.ie>
2015-04-01 19:41:37 +0000

    Database push

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Product.delete_all
-Product.create! id: 1, Title: "Banana", price: 0.49
-Product.create! id: 2, Title: "Apple", price: 0.29
-Product.create! id: 3, Title: "Carton of Strawberries", price: 1.99
+Product.create! id: 1, Title: "Banana", price: 0.49, Catagory: "fruit"
+Product.create! id: 2, Title: "Apple", price: 0.29, Catagory: "fruit"
+Product.create! id: 3, Title: "Carton of Strawberries", price: 1.99, Catagory: "sheep"
