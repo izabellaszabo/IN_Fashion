422c105ffc6f3570420840c5f0690b50d4803d13
Izabella Szabo <izabella.g.szabo@gmail.com>
2015-04-02 08:13:21 +0000

    Trying out Database work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
 Product.delete_all
-Product.create! id: 1, Title: "Banana", price: 0.49, Catagory: "fruit"
-Product.create! id: 2, Title: "Apple", price: 0.29, Catagory: "fruit"
-Product.create! id: 3, Title: "Carton of Strawberries", price: 1.99, Catagory: "sheep"
+#Product.create! id: 1, Title: "Banana", price: 0.49, Catagory: "fruit"
+#Product.create! id: 2, Title: "Apple", price: 0.29, Catagory: "fruit"
+#Product.create! id: 3, Title: "Carton of Strawberries", price: 1.99, Catagory: "sheep"
+Product.create! Title: "Mira Bella Fishtail Peplum Bandeau Dress", 
+  Description: "", Catagory: "womens", price: "34.00", Colour: "Black Yellow Grey"
+Product.create! Title: "", Description: "", Catagory: "", price: "", Colour: ""
+Product.create! Title: "", Description: "", Catagory: "", price: "", Colour: ""
+Product.create! Title: "", Description: "", Catagory: "", price: "", Colour: ""
+Product.create! Title: "", Description: "", Catagory: "", price: "", Colour: ""
+#Product.create! Title: "", Description: "", Catagory: "", price: "", Colour: ""
+#http://www.boohoo.com/icat/prom-shop
+
